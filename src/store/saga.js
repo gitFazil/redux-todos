d81274d2498a13c528ea74270c13f138d21a55c9
Mohd Fazil <kfazil185@gmail.com>
2022-03-15 18:42:26 +0530

    change delay min

--- src/store/saga.js
+++ src/store/saga.js
@@ -2,7 +2,7 @@ import { delay } from "redux-saga/effects";
 import { takeLatest, put } from "redux-saga/effects";
 
 function* addTodosAsync(data) {
-	yield delay(4000);
+	yield delay(500);
 	yield put({
 		type: "ADD_TODOS_ASYNC", payload: {
 			id: data.payload.id,
